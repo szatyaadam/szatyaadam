b6caf247542e878ec28cc81e9680a33078b33d2e
szatyaadam <szatyaadam87@gmail.com>
2023-01-19 13:51:15 +0100

    Vue atvalto program

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,14 @@
+<template>
+
+</template>
+<script setup>
+  import { ref } from 'vue' ;
+  const string=ref ("Átváltó");
+  
+  
+</script>
+
+
+<style scoped>
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import App from './form.items.vue'
+
+import './assets/main.css'
+
+createApp(App).mount('#app')
